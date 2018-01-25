12075b492e01302d88b9a5a927166f10861c7fca
Emmanuel Drouet <emmanuel.drouet@gmail.com>
2018-01-25 16:20:21 +0100

    remove policies

--- src/bundle/EdgarEzWebPushBundle.php
+++ src/bundle/EdgarEzWebPushBundle.php
@@ -3,7 +3,6 @@
 namespace Edgar\EzWebPushBundle;
 
 use Edgar\EzWebPushBundle\DependencyInjection\Security\PolicyProvider\WebPushPolicyProvider;
-use eZ\Bundle\EzPublishCoreBundle\DependencyInjection\EzPublishCoreExtension;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
@@ -11,8 +10,5 @@ class EdgarEzWebPushBundle extends Bundle
 {
     public function build(ContainerBuilder $container)
     {
-        /** @var EzPublishCoreExtension $eZExtension */
-        $eZExtension = $container->getExtension('ezpublish');
-        $eZExtension->addPolicyProvider(new WebPushPolicyProvider($this->getPath()));
     }
 }
